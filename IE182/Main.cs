7005b9cea017c4292f7962e7b7c7dc6ab4a7160f
yogy.phang <yogyphang@gmail.com>
2018-03-08 09:45:09 +0700

    Fix order and DB

--- IE182/Main.cs
+++ IE182/Main.cs
@@ -1,5 +1,4 @@
-﻿using LiteDB;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +16,7 @@ namespace IE182
 {
     public partial class Main : Form
     {
+        private List<Item> TmpItems { get; set; } = new List<Item>();
 
         public Main()
         {
@@ -45,88 +45,81 @@ namespace IE182
             var currentSheet = gridMain.CurrentWorksheet;
             var maxRow = currentSheet.RowCount;
             var maxCol = currentSheet.ColumnCount;
+            
+            TmpItems.Clear();
 
-            if (File.Exists("IE182.db"))
-                File.Delete("IE182.db");
-
-            // Open database (or create if not exits)
-            using (var db = new LiteDatabase("IE182.db"))
+            for (int i = 5; i <= maxRow; i++)
             {
-                // Get customer collection
-                var items = db.GetCollection<Item>("items");
-                
-                for (int i = 5; i <= maxRow; i++)
+                var firstCell = currentSheet[$"L{i}"];
+
+                if (firstCell == null || firstCell.ToString().Length == 0) //This empty data, ignore it
+                    break;
+
+                Invoke(new UpdateStatus(UpdateStat), $"Received Item : {i - 4}", 0);
+
+                var item = new Item
                 {
-                    var firstCell = currentSheet[$"L{i}"];
+                    A = currentSheet[$"A{i}"] != null ? currentSheet[$"A{i}"].ToString() : "",
+                    B = currentSheet[$"B{i}"] != null ? currentSheet[$"B{i}"].ToString() : "",
+                    C = currentSheet[$"C{i}"] != null ? currentSheet[$"C{i}"].ToString() : "",
+                    D = currentSheet[$"D{i}"] != null ? currentSheet[$"D{i}"].ToString() : "",
+                    E = currentSheet[$"E{i}"] != null ? currentSheet[$"E{i}"].ToString() : "",
+                    F = currentSheet[$"F{i}"] != null ? currentSheet[$"F{i}"].ToString() : "",
+                    G = currentSheet[$"G{i}"] != null ? currentSheet[$"G{i}"].ToString() : "",
+                    H = currentSheet[$"H{i}"] != null ? currentSheet[$"H{i}"].ToString() : "",
+                    I = currentSheet[$"I{i}"] != null ? currentSheet[$"I{i}"].ToString() : "",
+                    J = currentSheet[$"J{i}"] != null ? currentSheet[$"J{i}"].ToString() : "",
+                    K = currentSheet[$"K{i}"] != null ? currentSheet[$"K{i}"].ToString() : "",
+                    L = currentSheet[$"L{i}"] != null ? currentSheet[$"L{i}"].ToString() : "",
+                    M = currentSheet[$"M{i}"] != null ? currentSheet[$"M{i}"].ToString() : "",
+                    N = currentSheet[$"N{i}"] != null ? currentSheet[$"N{i}"].ToString() : "",
+                    O = currentSheet[$"O{i}"] != null ? currentSheet[$"O{i}"].ToString() : "",
+                    P = currentSheet[$"P{i}"] != null ? currentSheet[$"P{i}"].ToString() : "",
+                    Q = currentSheet[$"Q{i}"] != null ? currentSheet[$"Q{i}"].ToString() : "",
+                    R = currentSheet[$"R{i}"] != null ? currentSheet[$"R{i}"].ToString() : "",
+                    S = currentSheet[$"S{i}"] != null ? currentSheet[$"S{i}"].ToString() : "",
+                    T = currentSheet[$"T{i}"] != null ? currentSheet[$"T{i}"].ToString() : "",
+                    U = currentSheet[$"U{i}"] != null ? currentSheet[$"U{i}"].ToString() : "",
+                    V = currentSheet[$"V{i}"] != null ? currentSheet[$"V{i}"].ToString() : "",
+                    W = currentSheet[$"W{i}"] != null ? currentSheet[$"W{i}"].ToString() : "",
+                    X = currentSheet[$"X{i}"] != null ? currentSheet[$"X{i}"].ToString() : "",
+                    Y = currentSheet[$"Y{i}"] != null ? currentSheet[$"Y{i}"].ToString() : "",
+                    Z = currentSheet[$"Z{i}"] != null ? currentSheet[$"Z{i}"].ToString() : "",
+                    AA = currentSheet[$"AA{i}"] != null ? currentSheet[$"AA{i}"].ToString() : "",
+                    AB = currentSheet[$"AB{i}"] != null ? currentSheet[$"AB{i}"].ToString() : "",
+                    AC = currentSheet[$"AC{i}"] != null ? currentSheet[$"AC{i}"].ToString() : "",
+                    AD = currentSheet[$"AD{i}"] != null ? currentSheet[$"AD{i}"].ToString() : "",
+                    AE = currentSheet[$"AE{i}"] != null ? currentSheet[$"AE{i}"].ToString() : "",
+                    AF = currentSheet[$"AF{i}"] != null ? currentSheet[$"AF{i}"].ToString() : "",
+                    AG = currentSheet[$"AG{i}"] != null ? currentSheet[$"AG{i}"].ToString() : "",
+                    AH = currentSheet[$"AH{i}"] != null ? currentSheet[$"AH{i}"].ToString() : "",
+                    AI = currentSheet[$"AI{i}"] != null ? currentSheet[$"AI{i}"].ToString() : "",
+                    AJ = currentSheet[$"AJ{i}"] != null ? currentSheet[$"AJ{i}"].ToString() : "",
+                    AK = currentSheet[$"AK{i}"] != null ? currentSheet[$"AK{i}"].ToString() : "",
+                    AL = currentSheet[$"AL{i}"] != null ? currentSheet[$"AL{i}"].ToString() : "",
+                    AM = currentSheet[$"AM{i}"] != null ? currentSheet[$"AM{i}"].ToString() : "",
+                    AN = currentSheet[$"AN{i}"] != null ? currentSheet[$"AN{i}"].ToString() : "",
+                    AO = currentSheet[$"AO{i}"] != null ? currentSheet[$"AO{i}"].ToString() : "",
+                    AP = currentSheet[$"AP{i}"] != null ? currentSheet[$"AP{i}"].ToString() : "",
+                    AQ = currentSheet[$"AQ{i}"] != null ? currentSheet[$"AQ{i}"].ToString() : "",
+                    AR = currentSheet[$"AR{i}"] != null ? currentSheet[$"AR{i}"].ToString() : "",
+                    AS = currentSheet[$"AS{i}"] != null ? currentSheet[$"AS{i}"].ToString() : "",
+                    AT = currentSheet[$"AT{i}"] != null ? currentSheet[$"AT{i}"].ToString() : "",
+                    AU = currentSheet[$"AU{i}"] != null ? currentSheet[$"AU{i}"].ToString() : "",
+                    AV = currentSheet[$"AV{i}"] != null ? currentSheet[$"AV{i}"].ToString() : "",
+                    AW = currentSheet[$"AW{i}"] != null ? currentSheet[$"AW{i}"].ToString() : "",
+                    AX = currentSheet[$"AX{i}"] != null ? currentSheet[$"AX{i}"].ToString() : "",
+                    AY = currentSheet[$"AY{i}"] != null ? currentSheet[$"AY{i}"].ToString() : "",
+                    AZ = currentSheet[$"AZ{i}"] != null ? currentSheet[$"AZ{i}"].ToString() : "",
+                    BA = currentSheet[$"BA{i}"] != null ? currentSheet[$"BA{i}"].ToString() : "",
+                    BB = currentSheet[$"BB{i}"] != null ? currentSheet[$"BB{i}"].ToString() : "",
+                    BC = currentSheet[$"BC{i}"] != null ? currentSheet[$"BC{i}"].ToString() : ""
+                };
 
-                    if (firstCell == null || firstCell.ToString().Length == 0) //This empty data, ignore it
-                        break;
-
-                    Invoke(new UpdateStatus(UpdateStat), $"Received Item : {i - 4}", 0);
-
-                    var item = new Item
-                    {
-                        A = currentSheet[$"A{i}"] != null ? currentSheet[$"A{i}"].ToString() : "",
-                        B = currentSheet[$"B{i}"] != null ? currentSheet[$"B{i}"].ToString() : "",
-                        C = currentSheet[$"C{i}"] != null ? currentSheet[$"C{i}"].ToString() : "",
-                        D = currentSheet[$"D{i}"] != null ? currentSheet[$"D{i}"].ToString() : "",
-                        E = currentSheet[$"E{i}"] != null ? currentSheet[$"E{i}"].ToString() : "",
-                        F = currentSheet[$"F{i}"] != null ? currentSheet[$"F{i}"].ToString() : "",
-                        G = currentSheet[$"G{i}"] != null ? currentSheet[$"G{i}"].ToString() : "",
-                        H = currentSheet[$"H{i}"] != null ? currentSheet[$"H{i}"].ToString() : "",
-                        I = currentSheet[$"I{i}"] != null ? currentSheet[$"I{i}"].ToString() : "",
-                        J = currentSheet[$"J{i}"] != null ? currentSheet[$"J{i}"].ToString() : "",
-                        K = currentSheet[$"K{i}"] != null ? currentSheet[$"K{i}"].ToString() : "",
-                        L = currentSheet[$"L{i}"] != null ? currentSheet[$"L{i}"].ToString() : "",
-                        M = currentSheet[$"M{i}"] != null ? currentSheet[$"M{i}"].ToString() : "",
-                        N = currentSheet[$"N{i}"] != null ? currentSheet[$"N{i}"].ToString() : "",
-                        O = currentSheet[$"O{i}"] != null ? currentSheet[$"O{i}"].ToString() : "",
-                        P = currentSheet[$"P{i}"] != null ? currentSheet[$"P{i}"].ToString() : "",
-                        Q = currentSheet[$"Q{i}"] != null ? currentSheet[$"Q{i}"].ToString() : "",
-                        R = currentSheet[$"R{i}"] != null ? currentSheet[$"R{i}"].ToString() : "",
-                        S = currentSheet[$"S{i}"] != null ? currentSheet[$"S{i}"].ToString() : "",
-                        T = currentSheet[$"T{i}"] != null ? currentSheet[$"T{i}"].ToString() : "",
-                        U = currentSheet[$"U{i}"] != null ? currentSheet[$"U{i}"].ToString() : "",
-                        V = currentSheet[$"V{i}"] != null ? currentSheet[$"V{i}"].ToString() : "",
-                        W = currentSheet[$"W{i}"] != null ? currentSheet[$"W{i}"].ToString() : "",
-                        X = currentSheet[$"X{i}"] != null ? currentSheet[$"X{i}"].ToString() : "",
-                        Y = currentSheet[$"Y{i}"] != null ? currentSheet[$"Y{i}"].ToString() : "",
-                        Z = currentSheet[$"Z{i}"] != null ? currentSheet[$"Z{i}"].ToString() : "",
-                        AA = currentSheet[$"AA{i}"] != null ? currentSheet[$"AA{i}"].ToString() : "",
-                        AB = currentSheet[$"AB{i}"] != null ? currentSheet[$"AB{i}"].ToString() : "",
-                        AC = currentSheet[$"AC{i}"] != null ? currentSheet[$"AC{i}"].ToString() : "",
-                        AD = currentSheet[$"AD{i}"] != null ? currentSheet[$"AD{i}"].ToString() : "",
-                        AE = currentSheet[$"AE{i}"] != null ? currentSheet[$"AE{i}"].ToString() : "",
-                        AF = currentSheet[$"AF{i}"] != null ? currentSheet[$"AF{i}"].ToString() : "",
-                        AG = currentSheet[$"AG{i}"] != null ? currentSheet[$"AG{i}"].ToString() : "",
-                        AH = currentSheet[$"AH{i}"] != null ? currentSheet[$"AH{i}"].ToString() : "",
-                        AI = currentSheet[$"AI{i}"] != null ? currentSheet[$"AI{i}"].ToString() : "",
-                        AJ = currentSheet[$"AJ{i}"] != null ? currentSheet[$"AJ{i}"].ToString() : "",
-                        AK = currentSheet[$"AK{i}"] != null ? currentSheet[$"AK{i}"].ToString() : "",
-                        AL = currentSheet[$"AL{i}"] != null ? currentSheet[$"AL{i}"].ToString() : "",
-                        AM = currentSheet[$"AM{i}"] != null ? currentSheet[$"AM{i}"].ToString() : "",
-                        AN = currentSheet[$"AN{i}"] != null ? currentSheet[$"AN{i}"].ToString() : "",
-                        AO = currentSheet[$"AO{i}"] != null ? currentSheet[$"AO{i}"].ToString() : "",
-                        AP = currentSheet[$"AP{i}"] != null ? currentSheet[$"AP{i}"].ToString() : "",
-                        AQ = currentSheet[$"AQ{i}"] != null ? currentSheet[$"AQ{i}"].ToString() : "",
-                        AR = currentSheet[$"AR{i}"] != null ? currentSheet[$"AR{i}"].ToString() : "",
-                        AS = currentSheet[$"AS{i}"] != null ? currentSheet[$"AS{i}"].ToString() : "",
-                        AT = currentSheet[$"AT{i}"] != null ? currentSheet[$"AT{i}"].ToString() : "",
-                        AU = currentSheet[$"AU{i}"] != null ? currentSheet[$"AU{i}"].ToString() : "",
-                        AV = currentSheet[$"AV{i}"] != null ? currentSheet[$"AV{i}"].ToString() : "",
-                        AW = currentSheet[$"AW{i}"] != null ? currentSheet[$"AW{i}"].ToString() : "",
-                        AX = currentSheet[$"AX{i}"] != null ? currentSheet[$"AX{i}"].ToString() : "",
-                        AY = currentSheet[$"AY{i}"] != null ? currentSheet[$"AY{i}"].ToString() : "",
-                        AZ = currentSheet[$"AZ{i}"] != null ? currentSheet[$"AZ{i}"].ToString() : "",
-                        BA = currentSheet[$"BA{i}"] != null ? currentSheet[$"BA{i}"].ToString() : "",
-                        BB = currentSheet[$"BB{i}"] != null ? currentSheet[$"BB{i}"].ToString() : "",
-                        BC = currentSheet[$"BC{i}"] != null ? currentSheet[$"BC{i}"].ToString() : ""
-                    };
-
-                    items.Insert(item);
-                }
-                
+                TmpItems.Add(item);
             }
+
+            TmpItems = TmpItems.OrderBy(x => x.A).ThenBy(x => x.L).ToList();
 
             Invoke(new ClearTable(ResetGrid));
 
@@ -171,20 +164,12 @@ namespace IE182
 
         private void ProcessDB()
         {
-            //Check file Database
-            if (!File.Exists("IE182.db"))
-                return;
-
-            //Open Database
-            using (var db = new LiteDatabase("IE182.db"))
             using (var workbook = ReoGridControl.CreateMemoryWorkbook())
             {
                 try
                 {
                     var workshit = workbook.CreateWorksheet("Final");
-                    //Get Collections
-                    var collect = db.GetCollection<Item>("items");
-                    var items = collect.FindAll();
+                    var items = TmpItems;
                     var grp_material = (from a in items
                                         group a by new
                                         {
@@ -372,31 +357,7 @@ namespace IE182
             {
                 var po = item.POs.First();
 
-                AutoAppend(sheet, row_item, -1);
-
-                sheet[row_item, 0] = po.A;
-                sheet[row_item, 1] = "'" + po.B;
-                sheet[row_item, 2] = po.C;
-                sheet[row_item, 3] = "'" + po.D;
-                sheet[row_item, 4] = po.E;
-                sheet[row_item, 5] = po.F;
-                sheet[row_item, 6] = po.G;
-                sheet[row_item, 7] = po.H;
-                sheet[row_item, 8] = po.I;
-                sheet[row_item, 9] = po.J;
-                sheet[row_item, 10] = po.K;
-                sheet[row_item, 11] = "'" + po.L;
-                sheet[row_item, 12] = po.M;
-                sheet[row_item, 13] = "'" + po.N;
-                sheet[row_item, 14] = po.O;
-                sheet[row_item, 15] = "'" + po.P;
-                sheet[row_item, 16] = "'" + po.Q;
-                sheet[row_item, 17] = "'" + po.R;
-                sheet[row_item, 18] = po.S;
-                sheet[row_item, 19] = po.T;
-                sheet[row_item, 20] = po.U;
-                sheet[row_item, 21] = po.V;
-                sheet.SetRangeBorders(row_item, 0, 1, 22, BorderPositions.All, new RangeBorderStyle(Color.Black, BorderLineStyle.Solid));
+                AutoAppend(sheet, row_item + item.POs.Count - 1, -1);
 
                 foreach (var detPo in item.POs)
                 {
@@ -404,6 +365,28 @@ namespace IE182
 
                     if (col_x == -1) throw new Exception("Material Not Found!");
 
+                    sheet[row_item, 0] = po.A;
+                    sheet[row_item, 1] = "'" + po.B;
+                    sheet[row_item, 2] = po.C;
+                    sheet[row_item, 3] = "'" + po.D;
+                    sheet[row_item, 4] = po.E;
+                    sheet[row_item, 5] = po.F;
+                    sheet[row_item, 6] = po.G;
+                    sheet[row_item, 7] = po.H;
+                    sheet[row_item, 8] = po.I;
+                    sheet[row_item, 9] = po.J;
+                    sheet[row_item, 10] = po.K;
+                    sheet[row_item, 11] = "'" + po.L;
+                    sheet[row_item, 12] = po.M;
+                    sheet[row_item, 13] = "'" + po.N;
+                    sheet[row_item, 14] = po.O;
+                    sheet[row_item, 15] = "'" + po.P;
+                    sheet[row_item, 16] = "'" + po.Q;
+                    sheet[row_item, 17] = "'" + po.R;
+                    sheet[row_item, 18] = po.S;
+                    sheet[row_item, 19] = po.T;
+                    sheet[row_item, 20] = po.U;
+                    sheet[row_item, 21] = po.V;
                     sheet[row_item, col_x + 0] = detPo.AI;
                     sheet[row_item, col_x + 1] = detPo.AJ;
                     sheet[row_item, col_x + 2] = detPo.AK;
@@ -422,15 +405,18 @@ namespace IE182
                     sheet[row_item, col_x + 15] = detPo.BC;
 
                     sheet.SetRangeBorders(row_item, col_x, 1, 16, BorderPositions.All, new RangeBorderStyle(Color.Black, BorderLineStyle.Solid));
-                }
 
+                    row_item++;
+                }
+                
                 var progress = Convert.ToSingle(item_pos) / Convert.ToSingle(list_po.Count);
 
-                Invoke(new UpdateStatus(UpdateStat), $"Updating row {item_pos} / { list_po.Count }", progress);
-
-                row_item++;
+                Invoke(new UpdateStatus(UpdateStat), $"PO Processed {item_pos} / { list_po.Count }", progress);
+                
                 item_pos++;
             }
+
+            sheet.SetRangeBorders(14, 0, sheet.RowCount - 14, 22, BorderPositions.All, new RangeBorderStyle(Color.Black, BorderLineStyle.Solid));
         }
 
         private delegate void UpdateStatus(string status, float progress);
